4eb17c92964a5e3ee2443a670076c2c02be6abc4
anthonyg56 <anthonyg5256@gmail.com>
2020-01-29 09:46:35 -0500

    Prismic Plugin Fix, Still not working

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,21 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     {
+      resolve: 'gatsby-source-prismic-graphql',
+        options: {
+          repositoryName: 'creative-blog-gatsby', // (REQUIRED, replace with your own)
+          accessToken: 'MC5YakYzYVJFQUFDQUFkV0Fn.77-977-9Bu-_vTdJ77-9Fw8EfO-_ve-_vQoNUe-_ve-_vWwt77-9I--_vV_vv71877-977-977-9TO-_vWY', // (optional API access token)
+          /*path: '/preview', // (optional preview path. Default: /preview)
+          previews: true // (optional, activated Previews. Default: false)
+          pages: [{ // (optional, builds pages dynamically)
+            type: 'Article',         // TypeName from prismic
+            match: '/article/:uid',  // Pages will be generated under this pattern
+            path: '/article',        // Placeholder page for unpublished documents
+            component: require.resolve('./src/templates/article.js'),
+          }],*/
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
@@ -28,6 +43,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
